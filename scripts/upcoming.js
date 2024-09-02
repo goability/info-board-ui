4d3c5e06397fc2c8a4726a0a8db361a1cc39cc28
Matt Chandler <matt.chandleraz@gmail.com>
2024-09-02 11:11:57 -0500

    updates

--- scripts/upcoming.js
+++ scripts/upcoming.js
@@ -1,18 +1,18 @@
 let upcoming_data = [ 
     {
         "id": 1,
-        "title": "Clean out around gas pipes",
-        "description": "Gas company need access",
+        "title": "Ethan",
+        "description": "Mow grass, laundry",
         "start_date" : "2024-08-31",
-        "due_date": "2024-09-03 07:00:00",
+        "due_date": "2024-09-03",
         "completed": false,
         "image_large_url" : "",
         "image_small_url" : ""
     },
     {
         "id": 2,
-        "title": "Labor Day Holiday",
-        "description": "holiday",
+        "title": "Violet",
+        "description": "Litter box, kitchen table",
         "start_date" : "2024-09-02",
         "due_date": "2024-09-02",
         "completed": false,
@@ -21,8 +21,18 @@ let upcoming_data = [
     },
     {
         "id": 3,
-        "title": "Meet friends at park",
-        "description": "meet up with friends",
+        "title": "Olivia",
+        "description": "Take Practice test, dishes",
+        "start_date" : "2024-09-02",
+        "due_date": "2024-09-02",
+        "completed": false,
+        "image_large_url" : "",
+        "image_small_url" : ""
+    },
+    {
+        "id": 3,
+        "title": "Ivy",
+        "description": "Yard dog poop",
         "start_date" : "2024-09-02",
         "due_date": "2024-09-02",
         "completed": false,
@@ -43,9 +53,8 @@ function populateUpcomingList() {
         const listItem = document.createElement('li');
         listItem.innerHTML = `
         <div class='upcoming-item'>
-            <strong>${item.title}</strong>
+            <h1>${item.title}</h1>
             <p>${item.description}</p>
-            <p>${item.due_date}</p>
         </div>
         `;
         listItem.classList.add('upcoming-item');
