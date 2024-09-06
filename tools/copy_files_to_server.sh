14f54ad13be7cc2d39bb0413424c059f0b93a0d3
Matt Chandler <matt.chandleraz@gmail.com>
2024-09-06 17:55:08 -0500

    countdown

--- tools/copy_files_to_server.sh
+++ tools/copy_files_to_server.sh
@@ -9,10 +9,10 @@ echo $LOCAL_DIR
 REMOTE_SCRIPT="/home/matt/website/tools/info_board_refresh.sh"
 
 # Copy files to the server
-scp -r $LOCAL_DIR/index.html $USER@$SERVER:$REMOTE_DIR
-scp -r $LOCAL_DIR/scripts/* $USER@$SERVER:$REMOTE_DIR/scripts
-scp -r $LOCAL_DIR/css/* $USER@$SERVER:$REMOTE_DIR/css
-scp -r $LOCAL_DIR/images/* $USER@$SERVER:$REMOTE_DIR/images
-scp -r $LOCAL_DIR/data/* $USER@$SERVER:$REMOTE_DIR/data
+scp -i ~/.ssh/infoboard_local -r $LOCAL_DIR/index.html $USER@$SERVER:$REMOTE_DIR
+scp -i ~/.ssh/infoboard_local -r $LOCAL_DIR/scripts/* $USER@$SERVER:$REMOTE_DIR/scripts
+scp -i ~/.ssh/infoboard_local -r $LOCAL_DIR/css/* $USER@$SERVER:$REMOTE_DIR/css
+scp -i ~/.ssh/infoboard_local -r $LOCAL_DIR/images/* $USER@$SERVER:$REMOTE_DIR/images
+scp -i ~/.ssh/infoboard_local -r $LOCAL_DIR/data/* $USER@$SERVER:$REMOTE_DIR/data
 # Execute the script on the remote server
-ssh $USER@$SERVER "bash $REMOTE_SCRIPT"
+ssh -i ~/.ssh/infoboard_local $USER@$SERVER "bash $REMOTE_SCRIPT"
